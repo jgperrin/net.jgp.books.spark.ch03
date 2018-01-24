bd57fc42511d430d7d0d150fac538697d4b5165e
jgp <jgp@10.0.100.160>
2018-01-24 15:31:47 -0500

    Example of method chaining

---
+++ src/main/java/net/jgp/books/sparkWithJava/ch00/Object1.java
@@ -0,0 +1,10 @@
+package net.jgp.books.sparkWithJava.ch00;
+
+public class Object1 {
+
+  public Object2 getObject2() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ src/main/java/net/jgp/books/sparkWithJava/ch00/Object2.java
@@ -0,0 +1,10 @@
+package net.jgp.books.sparkWithJava.ch00;
+
+public class Object2 {
+
+  public void set(String string) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}

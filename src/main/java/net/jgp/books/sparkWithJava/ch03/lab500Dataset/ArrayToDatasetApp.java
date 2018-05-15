e350f108263b319dd396fcfb8c2a4b6760bcfe2b
Jean Georges Perrin <jgp@jgp.net>
2018-05-15 04:31:41 -0400

    Added ArrayToDatasetApp

---
+++ src/main/java/net/jgp/books/sparkWithJava/ch03/lab500Dataset/ArrayToDatasetApp.java
@@ -0,0 +1,33 @@
+package net.jgp.books.sparkWithJava.ch03.lab500Dataset;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * Converts an array to a Dataset
+ * 
+ * @author jperrin
+ */
+public class ArrayToDatasetApp {
+
+  public static void main(String[] args) {
+    ArrayToDatasetApp app = new ArrayToDatasetApp();
+    app.start();
+  }
+
+  private void start() {
+    SparkSession spark = SparkSession.builder()
+        .appName("Array to Dataset<String>")
+        .master("local")
+        .getOrCreate();
+
+    String[] l = new String[] { "a", "b", "c", "d" };
+    List<String> data = Arrays.asList(l);
+    Dataset<String> df = spark.createDataset(data, Encoders.STRING());
+    df.show();
+  }
+}
---
+++ src/main/java/net/jgp/books/sparkWithJava/ch03/lab510DatasetToDataframe/CsvToDatasetBookToDataframeApp.java
@@ -0,0 +1,63 @@
+package net.jgp.books.sparkWithJava.ch03.lab510DatasetToDataframe;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import net.jgp.books.sparkWithJava.ch03.x.model.Book;
+
+public class CsvToDatasetBookToDataframeApp implements Serializable {
+  private static final long serialVersionUID = 4262746489728980066L;
+
+  class BookMapper implements MapFunction<Row, Book> {
+    private static final long serialVersionUID = -8940709795225426457L;
+
+    @Override
+    public Book call(Row value) throws Exception {
+      Book b = new Book();
+      b.setId(value.getAs("id"));
+      b.setAuthorId(value.getAs("authorId"));
+      b.setLink(value.getAs("link"));
+      SimpleDateFormat parser = new SimpleDateFormat("M/d/yy");
+      String stringAsDate = value.getAs("releaseDate");
+      if (stringAsDate == null) {
+        b.setReleaseDate(null);
+      } else {
+        b.setReleaseDate(parser.parse(stringAsDate));
+      }
+      b.setTitle(value.getAs("title"));
+      return b;
+    }
+  }
+
+  public static void main(String[] args) {
+    CsvToDatasetBookToDataframeApp app = new CsvToDatasetBookToDataframeApp();
+    app.start();
+  }
+
+  private void start() {
+    SparkSession spark = SparkSession.builder().appName("CSV to Dataset<Book>")
+        .master("local").getOrCreate();
+
+    String filename = "data/books.csv";
+    Dataset<Row> df = spark.read().format("csv")
+        .option("inferSchema", "true")
+        .option("header", "true")
+        .load(filename);
+    df.show(5);
+    df.printSchema();
+
+    Dataset<Book> bookDs = df.map(new BookMapper(), Encoders.bean(Book.class));
+    bookDs.show(5);
+    bookDs.printSchema();
+
+    Dataset<Row> df2 = bookDs.toDF();
+    df2.show(5);
+    df2.printSchema();
+  }
+}

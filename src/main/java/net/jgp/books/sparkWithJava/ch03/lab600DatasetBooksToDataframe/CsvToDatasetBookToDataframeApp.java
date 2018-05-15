57a556a42e191f2679c4e165813f335efacee25b
Jean Georges Perrin <jgp@jgp.net>
2018-05-15 08:23:37 -0400

    Comments...

--- src/main/java/net/jgp/books/sparkWithJava/ch03/lab600DatasetBooksToDataframe/CsvToDatasetBookToDataframeApp.java
+++ src/main/java/net/jgp/books/sparkWithJava/ch03/lab600DatasetBooksToDataframe/CsvToDatasetBookToDataframeApp.java
@@ -1,5 +1,10 @@
 package net.jgp.books.sparkWithJava.ch03.lab600DatasetBooksToDataframe;
 
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.expr;
+import static org.apache.spark.sql.functions.lit;
+import static org.apache.spark.sql.functions.to_date;
+
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 
@@ -11,11 +16,23 @@ import org.apache.spark.sql.SparkSession;
 
 import net.jgp.books.sparkWithJava.ch03.x.model.Book;
 
+/**
+ * This example will read a CSV file, ingest it in a dataframe, convert the
+ * dataframe to a dataset, and vice versa.
+ * 
+ * @author jgp
+ */
 public class CsvToDatasetBookToDataframeApp implements Serializable {
-  private static final long serialVersionUID = 4262746489728980066L;
+  private static final long serialVersionUID = -1L;
 
+  /**
+   * This is a mapper class that will convert a Row to an instance of Book. You
+   * have full control over it - isn't it great that sometimes you have control?
+   * 
+   * @author jgp
+   */
   class BookMapper implements MapFunction<Row, Book> {
-    private static final long serialVersionUID = -8940709795225426457L;
+    private static final long serialVersionUID = -2L;
 
     @Override
     public Book call(Row value) throws Exception {
@@ -35,11 +52,20 @@ public class CsvToDatasetBookToDataframeApp implements Serializable {
     }
   }
 
+  /**
+   * It starts here
+   * 
+   * @param args
+   */
   public static void main(String[] args) {
-    CsvToDatasetBookToDataframeApp app = new CsvToDatasetBookToDataframeApp();
+    CsvToDatasetBookToDataframeApp app =
+        new CsvToDatasetBookToDataframeApp();
     app.start();
   }
 
+  /**
+   * All the work is done here.
+   */
   private void start() {
     SparkSession spark = SparkSession.builder().appName("CSV to Dataset<Book>")
         .master("local").getOrCreate();
@@ -49,14 +75,32 @@ public class CsvToDatasetBookToDataframeApp implements Serializable {
         .option("inferSchema", "true")
         .option("header", "true")
         .load(filename);
+
+    System.out.println("*** Books ingested in a dataframe");
     df.show(5);
     df.printSchema();
 
     Dataset<Book> bookDs = df.map(new BookMapper(), Encoders.bean(Book.class));
-    bookDs.show(5);
+    System.out.println("*** Books are now in a dataset of books");
+    bookDs.show(5, 17);
     bookDs.printSchema();
 
     Dataset<Row> df2 = bookDs.toDF();
+    df2 = df2.withColumn(
+        "releaseDateAsString",
+        concat(
+            expr("releaseDate.year + 1900"), lit("-"),
+            expr("releaseDate.month + 1"), lit("-"),
+            df2.col("releaseDate.date")));
+    // Although you are getting a date out this process (pretty cool, huh?),
+    // this is not the recommended way to get a date. Have a look at chapter 7
+    // on ingestion for better ways.
+    df2 = df2
+        .withColumn(
+            "releaseDateAsDate",
+            to_date(df2.col("releaseDateAsString"), "yyyy-MM-dd"))
+        .drop("releaseDateAsString");
+    System.out.println("*** Books are back in a dataframe");
     df2.show(5);
     df2.printSchema();
   }
